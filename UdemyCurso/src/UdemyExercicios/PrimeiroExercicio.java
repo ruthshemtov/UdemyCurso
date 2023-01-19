93f8625c891074f9f10785c15850065e20613b2b
ruth santos <ruthsantos86@gmail.com>
2023-01-18 20:11:15 -0400

    principal

---
+++ UdemyCurso/src/UdemyExercicios/PrimeiroExercicio.java
@@ -0,0 +1,23 @@
+package UdemyExercicios;
+
+import java.util.Scanner;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+		
+		int valor_um = sc.nextInt();
+		int valor_dois = sc.nextInt();
+		
+		int soma = valor_um + valor_dois;
+		
+		System.out.println("SOMA = " + soma);
+		
+		sc.close();
+		
+
+	}
+
+}
